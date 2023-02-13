2883544b64c62a980f16ebc4daf3db3c285304af
hamza najji <najjimos0@gmail.com>
2023-02-13 17:17:10 +0100

    Adding Column Names Based On Pojo to Excel

--- src/main/java/org/zaham/jexcel/mapper/MapFileToEntityImpl.java
+++ src/main/java/org/zaham/jexcel/mapper/MapFileToEntityImpl.java
@@ -10,35 +10,56 @@ import org.zaham.jexcel.factory.CellsFactory;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class MapFileToEntityImpl implements MapFileToEntity {
-
-    @SneakyThrows
-    public <T> void mapFileToObject(List<Cell> cells, Class<T> clazz) {
-        T object = clazz.newInstance();
-
-    }
 
     @SneakyThrows
     @SuppressWarnings("java:S3011")
     @Override
     public <T> void mapEntityToFile(Sheet sheet,List<T> entities,boolean firstRowHeader){
         AtomicInteger rowIndex = new AtomicInteger();
+        AtomicBoolean setColumnName = new AtomicBoolean(firstRowHeader);
+        AtomicReference<Row> row = new AtomicReference<>(sheet.createRow(rowIndex.getAndIncrement()));
+        T e = entities.get(0);
+        Field[] declaredFields = e.getClass().getDeclaredFields();
+        if(setColumnName.get()){
+            AtomicInteger columnCells = new AtomicInteger();
+            generateExcelHeader(declaredFields)
+                    .forEach(s -> {
+                        Cell cell = row.get().createCell(columnCells.get());
+                        CellsFactory.setCellFactry(cell,s);
+                        columnCells.getAndIncrement();
+                    });
+            setColumnName.set(false);
+            //rowIndex.getAndIncrement();
+        }
         entities.forEach( entity ->{
-            Row row = sheet.createRow(rowIndex.getAndIncrement());
-            Field[] declaredFields = entity.getClass().getDeclaredFields();
+            row.set(sheet.createRow(rowIndex.getAndIncrement()));
             AtomicInteger numberOfCells = new AtomicInteger();
+            if(setColumnName.get()){
+                generateExcelHeader(declaredFields)
+                        .forEach(s -> {
+                            Cell cell = row.get().createCell(numberOfCells.get());
+                            CellsFactory.setCellFactry(cell,s);
+                            numberOfCells.getAndIncrement();
+                        });
+                setColumnName.set(false);
+                sheet.createRow(rowIndex.getAndIncrement());
+            }
             Arrays.stream(declaredFields)
                     .forEach(field -> {
                         field.setAccessible(true);
                         try {
                             Object value = field.get(entity);
-                            Cell cell = row.createCell(numberOfCells.get());
+                            Cell cell = row.get().createCell(numberOfCells.get());
                             CellsFactory.setCellFactry(cell,value);
                             numberOfCells.getAndIncrement();
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException(e);
+                        } catch (IllegalAccessException ex) {
+                            throw new RuntimeException(ex);
                         }
                     });
         });
@@ -58,5 +79,10 @@ public class MapFileToEntityImpl implements MapFileToEntity {
                     });
         }
         throw new UnsupportedOperationException("method not implemented yet");
+    }
+    public <T> List<String> generateExcelHeader(Field[] fields){
+         return Arrays.stream(fields)
+                .map(Field::getName)
+                .collect(Collectors.toList());
     }
 }
