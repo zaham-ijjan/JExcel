aa0abed0ec3ae8fd0349e733c964be1b57005649
zaham-ijjan <najjimos0@gmail.com>
2023-02-16 19:28:03 +0100

    Adding documentation to the file

--- src/test/java/org/zaham/jexcel/JExcelTest.java
+++ src/test/java/org/zaham/jexcel/JExcelTest.java
@@ -5,7 +5,6 @@ import lombok.SneakyThrows;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.zaham.jexcel.core.pojo.Employees;
 import org.zaham.jexcel.enums.ExcelType;
@@ -19,8 +18,24 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.zaham.jexcel.util.FileUtil.getResourceFileAsString;
 
 @ExtendWith(MockitoExtension.class)
-class JExcelFactoryTest {
+class JExcelTest {
 
+
+    @Test
+    @SneakyThrows
+    @Disabled("we dont want to generate the file in hard disk enbale it only in dev")
+    void writeEntityWithFile() {
+        //given
+        String employees = getResourceFileAsString("excel/employees.json");
+        Gson gson = new Gson();
+        List<Employees> employeesList = Arrays.asList(gson.fromJson(employees,Employees[].class));
+        String path = "";
+        ExcelType excelType = ExcelType.XLSX;
+        boolean enableColumn = true;
+        JExcel<Employees> jExcel = new JExcel<>();
+        File File =  jExcel.excelFileGenerator(employeesList,excelType,enableColumn,path).get();
+        assertNotNull(File);
+    }
 
     @Test
     @SneakyThrows
@@ -29,11 +44,10 @@ class JExcelFactoryTest {
         String employees = getResourceFileAsString("excel/employees.json");
         Gson gson = new Gson();
         List<Employees> employeesList = Arrays.asList(gson.fromJson(employees,Employees[].class));
-        String path = "";
         ExcelType excelType = ExcelType.XLSX;
         boolean enableColumn = true;
-        JExcelFactory<Employees> jExcelFactory = new JExcelFactory<>();
-        OutputStream outputStream =  jExcelFactory.excelByteArrayGenerator(employeesList,excelType,enableColumn,path).get();
+        JExcel<Employees> jExcel = new JExcel<>();
+        OutputStream outputStream =  jExcel.excelByteArrayGenerator(employeesList,excelType,enableColumn).get();
         assertNotNull(outputStream);
     }
 }
